2a0804a7138f0e05a68797cd059dbde75ca95b41
Darius Krištapavičius <darius@darneta.lt>
2019-01-27 12:58:30 +0200

    Made Access-Control-Allow-Methods met rfc.

--- api/src/EventListener/KernelResponseCORSHeaders.php
+++ api/src/EventListener/KernelResponseCORSHeaders.php
@@ -11,6 +11,6 @@ class KernelResponseCORSHeaders
         $response = $event->getResponse();
         $response->headers->set('Access-Control-Allow-Origin', '*', true);
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type', true);
-        $response->headers->set('Access-Control-Allow-Methods', '*', true);
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PATCH', true);
     }
 }
